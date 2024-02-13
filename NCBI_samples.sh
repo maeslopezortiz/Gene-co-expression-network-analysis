9fd8311ff9c2e55a742955413d87c40d04dd31e6
María Estefanía López Ortiz <148332361+maeslopezortiz@users.noreply.github.com>
2024-02-13 17:22:27 +0100

    NCBI_samples.sh

--- NCBI_samples.sh
+++ NCBI_samples.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# Slurm directives:
+#SBATCH --cpus-per-task 4
+#SBATCH --mem-per-cpu 4G
+#SBATCH --time 1-5:10
+#SBATCH --output=logs/NCBIdownload.out
+#SBATCH --error=logs/NCBIdownload.err
+#SBATCH --job-name=NCBIdownloading
 # Define the input file with the names of RNAseq samples
 SAMPLES=~/RNA_samples_names.txt  # txt file with the names of the RNA sample accessions from NCBI
 
